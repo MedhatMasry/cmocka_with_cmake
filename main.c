c612a38e5798e4c5e7b6a1ee38005dd5834f9bcd
Medhat Masry <105849363+MedhatMasry@users.noreply.github.com>
2023-09-22 21:33:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -22,16 +22,26 @@
 #include <stddef.h>
 #include <cmocka.h>
 
-static void test(void **state)
+static void test1(void **state)
 {
     assert_int_equal(2, 2);
+}
+static void test2(void **state)
+{
+    assert_int_equal(1, 2);
+}
+static void test3(void **state)
+{
+    assert_int_equal(10, 10);
 }
 
 int main()
 {
     const struct CMUnitTest tests[] =
     {
-        cmocka_unit_test(test),
+        cmocka_unit_test(test1),
+        cmocka_unit_test(test2),
+        cmocka_unit_test(test3),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
